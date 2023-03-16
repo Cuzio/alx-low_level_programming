695fb60952e97e1334f1f0f31b605ce142bf7e4c
Cuzio <itomax19@gmail.com>
2023-03-16 10:31:11 -0700

    using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - a program the prints exactly what was inputted.
+ * Return: 0 (to show that it was successful)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
