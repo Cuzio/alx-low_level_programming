f14a7212a48ef2a47ae01b54e231b8593f37cad7
Cuzio <itomax19@gmail.com>
2023-03-16 13:47:45 -0700

    unix quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - unix program that prints exactly what it is needed.
+ * Return: 1 (shows that it is successful)
+ */
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
+}
