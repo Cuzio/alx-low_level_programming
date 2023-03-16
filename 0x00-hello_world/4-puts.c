3e948c76624532b145b86c38bba342e70b2f9c7a
Cuzio <itomax19@gmail.com>
2023-03-16 10:23:23 -0700

    puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - The program that will print exactly input given to it.
+ * Return: 0 (To show that it was successful.
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
