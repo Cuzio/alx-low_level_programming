b0a401df499746eaae5c233098d1476c4e549569
Cuzio <itomax19@gmail.com>
2023-03-16 11:46:42 -0700

    printing the sizes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,11 +5,16 @@
  */
 int main(void)
 {
-	int i;
-	double d;
 	char c;
-	printf("The size of an int is : %lu.\n",(unsigned long)sizeof(i));
-	printf("The size of an double is : %lu.\n",(unsigned long)sizeof(d));
-	printf("The size of an c is : %lu.\n",(unsigned long)sizeof(c));
+	int i;
+	long int l;
+	long long int d;
+	float f;
+
+	printf("Size of a char is : %lu.\n", (unsigned long)sizeof(c));
+	printf("Size of an int is : %lu.\n", (unsigned long)sizeof(i));
+	printf("Size of a long int is : %lu.\n", (unsigned long)sizeof(l));
+	printf("Size of a long long int is : %lu.\n", (unsigned long)sizeof(d));
+	printf("Size of a float is : %lu.\n", (unsigned long)sizeof(f));
 	return (0);
 }
