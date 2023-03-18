2809af6f86a7637547e3a7c8824ed96a0a108f71
Cuzio <itomax19@gmail.com>
2023-03-17 19:15:58 -0700

    mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.>
+
+/**
+ * main - mirror, let's give your something to look at.
+ * Return: always 0
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'z'; c >= 'a'; a--)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
