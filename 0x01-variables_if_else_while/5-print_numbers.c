9cae2d7c3ecc7cbe7a5a13379221c69ba087676a
Cuzio <itomax19@gmail.com>
2023-03-17 17:18:35 -0700

    print numbers from 0 to 9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - print all single digits in base 10.
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		printf("%d\n", n);
+	}
+	return (0);
+}
