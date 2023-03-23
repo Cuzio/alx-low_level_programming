cff5b1654eaeab0fae8b3615cd0f7e4fe38ce36a
Cuzio <itomax19@gmail.com>
2023-03-23 05:55:59 -0700

    multiple

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - the function to use to multiple
+ * @a: variable
+ * @b: variable
+ * Return: always 0
+ */
+
+int mul(int a, int b)
+{
+	mul();
+
+	mul = a * b;
+	return (mul);
+}
