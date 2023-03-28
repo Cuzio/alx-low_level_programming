131d452567242dc24c52923aceff20d873686346
Cuzio <itomax19@gmail.com>
2023-03-28 01:43:54 -0700

    reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * print_rev - to print the reverse
+ * @*s: string to print
+ * Return: 0
+ */
+
+void print_rev(char *s)
+{
+	int len = strlen(s);
+
+	while (len--)
+		putchar(*(s + len));
+	putchar(10);
+}
