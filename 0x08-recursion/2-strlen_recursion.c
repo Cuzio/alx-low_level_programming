037e2a8db82410be050a16f2d03ef6e10ce01837
Cuzio <itomax19@gmail.com>
2023-04-05 15:48:22 -0700

    hard

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,17 +1,16 @@
-#include<stdio.h>
+#include <stdio.h>
 #include "main.h"
 
 /**
- * _print_rev_recursion - prints a string in reverse.
- * @s: string to be printed
- * Return: always 0
+ * _strlen_recursion - returns the length of a string.
+ * @s: string to be printed.
+ * Return: the length of a string.
  */
 
-void _print_rev_recursion(char *s)
+int _strlen_recursion(char *s)
 {
-	if (*s != '\0')
-	{
-		_print_rev_recursion(s + 1);
-		_putchar(*s);
-	}
+	if (*s == '\0')
+		return (0);
+	else
+		return (1 + _strlen_recursion(s + 1));
 }
