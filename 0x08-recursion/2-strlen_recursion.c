e2c6456481bd511faf094654538ab0302011dc2c
Cuzio <itomax19@gmail.com>
2023-04-05 15:42:53 -0700

    hard

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
-#include "maom.hi
+#include "main.h"
 
 /**
  * _print_rev_recursion - prints a string in reverse.
- * @s: string
- * Return: no return.
+ * @s: string to be printed
+ * Return: always 0
  */
 
 void _print_rev_recursion(char *s)
