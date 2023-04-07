164e6c0ae936a9fe1bf1afbbf6050ed955c7e975
Cuzio <itomax19@gmail.com>
2023-04-07 11:12:11 -0700

    name of the file

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main -  program that prints its name
+ * @argc: the number of things entered
+ * @argv: the array that holds the string
+ * Return: 0
+ */
+
+int main (int argc, char* argv[])
+{
+	(void) argc;
+
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
