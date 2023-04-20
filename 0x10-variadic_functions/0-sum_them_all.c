3e5cd049ba38d6e133167ac368dd4cabcc836c0c
Cuzio <itomax19@gmail.com>
2023-04-20 15:26:23 -0700

    sum

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,30 @@
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - function to be added
+ * @n: variable
+ * Return: 0
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list list;
+	unsigned int i;
+	int sum = 0;
+
+	if (n == 0)
+	{
+		return (0);
+	}
+
+	va_start(list, n);
+	
+	for (i = 0; i < n; i++)
+	{
+		sum += va_args(list, int);
+	}
+
+	va_end(list);
+
+	return (sum);
+}
